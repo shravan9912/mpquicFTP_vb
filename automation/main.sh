94a9d3d16e8808690f553b5418963325f423da8d
shravan9912 <shravan9912@gmail.com>
2023-05-20 10:20:53 +0530

    shravan

--- automation/main.sh
+++ automation/main.sh
@@ -1,5 +1,6 @@
 export GOPRIVATE=github.com/shravan9912
 #go build server.go
 #cd client && go build client.go && cd ..
+cd ..
 set -a && source envs/mininet.env && set +a
 sudo python mininettest/topo2_1.py
